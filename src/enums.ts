00a4883d1b506925c13cf463af8b74430a56da94
Igor Chernov <igorek_chernov_1998@mail.ru>
2026-01-07 13:07:46 +0300

    add new enum

--- src/enums.ts
+++ src/enums.ts
@@ -3,5 +3,6 @@ export enum HttpStatusCode {
     Created_201 = 201,
     NoContent_204 = 204,
     BadRequest_400 = 400,
-    NotFound_404 = 404
+    NotFound_404 = 404,
+    Incorrect_422 = 422,
 }
